a4637d793cba24eaad2b416ccf8f55f72fc1a8e8
JustAlex975 <123792957+JustAlex975@users.noreply.github.com>
2023-01-27 23:19:54 -0700

    Update Better-Grimoire.js

--- Better-Grimoire.js
+++ Better-Grimoire.js
@@ -1,22 +1,29 @@
-if(MyMod === undefined) var MyMod = {};
-if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+Game.Win('Third-party');
+if(BetterGrimoire === undefined) var BetterGrimoire = {};
+if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/' + (0 ? 'Beta/' : '') + 'CCSE.js');
+BetterGrimoire.name = 'Better Grimoire';
+BetterGrimoire.pic = 'https://klattmose.github.io/CookieClicker/img/timer.png';
+BetterGrimoire.version = '0.0';
+BetterGrimoire.GameVersion = '2.048';
 
-MyMod.launch = function(){
-	/**
-	
-	All the code you want to delay goes here
-	Put "MyMod.isLoaded = 1;" somewhere within
-	
-	**/
+BetterGrimoire.launch = function(){
+	BetterGrimoire.isLoaded = 1;
+	CCSE.MinigameReplacer(function(){
+		var objKey = 'Wizard tower';
+		var M = Game.Objects[objKey].minigame;
+		
+		//I'll work this out later
+		
+	}, 'Wizard tower');
 }
 
-if(!MyMod.isLoaded){
+if(!BetterGrimoire.isLoaded){
 	if(CCSE && CCSE.isLoaded){
-		MyMod.launch();
+		BetterGrimoire.launch();
 	}
 	else{
 		if(!CCSE) var CCSE = {};
 		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
-		CCSE.postLoadHooks.push(MyMod.launch);
+		CCSE.postLoadHooks.push(BetterGrimoire.launch);
 	}
 }
